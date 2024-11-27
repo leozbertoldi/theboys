2f6adb6152280e7e97d047ed799a0f82d691263b
leozbertoldi <leozbertoldi@gmail.com>
2024-11-27 13:28:28 -0300

    [NEW] Att

--- entidades.c
+++ entidades.c
@@ -2,3 +2,30 @@
 #include <stdlib.h>
 #include "entidades.h"
 
+struct mundo *inicializa_mundo(struct mundo *m)  
+{
+  struct heroi h;
+
+  if (!m)
+    return NULL;
+ 
+  for (i = 0; i < m->nherois; i++)
+  { 
+    h = malloc(sizeof(h));
+    
+    if (!h)
+      return NULL;
+
+    inicializa_heroi(h, i);
+    m->herois[i] = h;
+  }
+}
+
+struct heroi *inicializa_heroi(struct heroi *h, int i)
+{
+  h->ID = i;
+  h->experiencia = 0;
+  h->paciencia = rand() % 101; //aleat entre 0 e 100
+  h->velocidade = rand() % 4951 + 50;  //aleat entre 50 e 5000
+  return h;
+}
--- theboys.c
+++ theboys.c
@@ -9,34 +9,18 @@
 #include "entidades.h"
 #include "eventos.h"
 
-#define
+#define T_INICIO 0
+#define T_FIM_DO_MUNDO 525600
+#define N_TAMANHO_MUNDO 20000
+#define N_HABILIDADES 10
 
 // programa principal
 int main ()
 {
   // iniciar o mundo
   struct mundo m;
-  struct heroi h;
-  int t_inicial, t_final, i;
   
   srand(0);
-
-  t_inicial = 0;
-  t_final = 525600;
-  m->tamanho = 20000;
-  m->nhabilidades = 10;
-  m->nherois = m->nhabilidades*5;
-  m->nbases = m->nherois/5;
-  m->nmissoes = t_final/100;
-
-  for (i = 0; i < m->nherois; i++)
-  {
-    h->ID = i;
-    h->experiencia = 0;
-    h->paciencia = rand() % 101; //aleat entre 0 e 100
-    h->velocidade = rand() % 4951 + 50;  //aleat entre 50 e 5000
-    
-    m->herois[i] = h;
 
   // executar o laço de simulação
 
